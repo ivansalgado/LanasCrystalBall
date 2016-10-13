f18907ca2337a6647d6c4e0287d444f029cd87ef
Ivaaaaaaaaaaan <ivan@email.com>
2016-10-13 00:05:36 -0700

    dis my project :kiss:
    there may or may not be errors :wink:

--- app/src/main/java/com/example/student/lanascrystalball/Songs.java
+++ app/src/main/java/com/example/student/lanascrystalball/Songs.java
@@ -2,92 +2,32 @@ package com.example.student.lanascrystalball;
 
 public class Songs {
 
-    private static Songs predictions;
-    private String[] answers;
+    private static Songs songs;
+    private String[] choice;
 
     private Songs() {
-        answers = new String[] {
+        choice = new String[] {
                 "Ultraviolence",
-                "Born to Die"
+                "Honeymoon",
+                "Lolita",
+                "Video Games",
+                "Brooklyn Baby",
+                "Ride",
+                "Freak",
+                "Carmen",
+                "Shades of Cool",
+                "West Coast"
         };
     }
 
     public static Songs get() {
-        if(predictions == null) {
-            predictions = new Songs();
+        if(songs == null) {
+            songs = new Songs();
         }
-        return predictions;
+        return songs;
     }
 
     public String getSong() {
-        return answers[1];
+        return choice[1];
     }
 }
-
-MainActivity.java
-        package com.example.student.lanascrystalball;
-
-        import android.content.Context;
-        import android.hardware.Sensor;
-        import android.hardware.SensorEvent;
-        import android.hardware.SensorEventListener;
-        import android.hardware.SensorManager;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
-
-    private TextView answerText;
-
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-
-    private float acceleration;
-    private float currentAcceleration;
-    private float previousAcceleration;
-
-    private final SensorEventListener sensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        acceleration = 0.0f;
-        currentAcceleration = SensorManager.GRAVITY_EARTH;
-        previousAcceleration = SensorManager.GRAVITY_EARTH;
-
-        answerText = (TextView) findViewById(R.id.answerText);
-        answerText.setText(Predictions.get().getPrediction());
-        answerText.setText("You have recieved FourArms");
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(sensorListener);
-    }
-}
-
-styles.xml
